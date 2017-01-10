49aa030715e47efc5c5ba198dc98ff5d7b5d2cb5
angalle <gmltjs150@naver.com>
2017-01-10 23:21:53 +0900

    image animation ratation

--- app/src/main/java/univ/sm/SchDetail.java
+++ app/src/main/java/univ/sm/SchDetail.java
@@ -1,6 +1,8 @@
 package univ.sm;
 
 import android.animation.ObjectAnimator;
+import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -26,9 +28,9 @@ import java.util.logging.Logger;
  */
 
 public class SchDetail extends AppCompatActivity implements View.OnClickListener,ViewTreeObserver.OnGlobalLayoutListener {
-    ImageView schDetailTopBar;
+    ImageView schDetailTopBar,quickBtn;
     TextView schDetailWeekDay,schDetailSatureDay,schDetailSunDay;
-
+    int roatationDegree = 0;
 
 
     @Override
@@ -38,65 +40,52 @@ public class SchDetail extends AppCompatActivity implements View.OnClickListener
         schDetailWeekDay = (TextView) findViewById(R.id.sch_detail_weekDay);
         schDetailSatureDay = (TextView) findViewById(R.id.sch_detail_satureDay);
         schDetailSunDay = (TextView) findViewById(R.id.sch_detail_sunDay);
+        quickBtn = (ImageView) findViewById(R.id.quickBtn);
 
+        /* 기본좌표 재 설정 / xml 상에서 정확한 좌표를 표시 할 수 없음*/
         schDetailTopBar = (ImageView) findViewById(R.id.sch_detail_top_bar);
-
-        //schDetailTopBar.setX(schDetailWeekDay.getX());
-
-        System.out.println("==================================");
-        System.out.println(schDetailWeekDay.getX());
-        System.out.println(schDetailWeekDay.getY());
-
         schDetailTopBar.getViewTreeObserver().addOnGlobalLayoutListener(this);
 
+
+        /* 각 버튼 마다 이벤트 리스너*/
         schDetailWeekDay.setOnClickListener(this);
         schDetailSatureDay.setOnClickListener(this);
         schDetailSunDay.setOnClickListener(this);
+        quickBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        float toX=0,toWidth=0;
+        float toX=0,toWidth=0;      /* 이동해야할 x좌표 */
         if(v.getId() == R.id.sch_detail_weekDay){
             toX = v.getLeft();
             toWidth = v.getWidth();
+            moveImageBar(toWidth,toX);
         }else if(v.getId() == R.id.sch_detail_satureDay){
             toX = v.getLeft();
             toWidth = v.getWidth();
+            moveImageBar(toWidth,toX);
         }else if(v.getId() == R.id.sch_detail_sunDay){
             toX = v.getLeft();
             toWidth = v.getWidth();
+            moveImageBar(toWidth,toX);
+        }else if(v.getId() == R.id.quickBtn){
+            roatationDegree += 360;
+            ObjectAnimator rotation = ObjectAnimator.ofFloat(quickBtn,"rotation", roatationDegree).setDuration(1000);
+            rotation.setRepeatCount(Animation.ABSOLUTE);
+            rotation.start();
         }
+    }
 
-
+    private void moveImageBar(float toWidth,float toX){
         float width = schDetailTopBar.getWidth();
-        System.out.println(width);
-        System.out.println(toX);
-
         schDetailTopBar.animate().scaleX(toWidth/width);
-
-
         schDetailTopBar.animate().translationX(toX-(width-toWidth)/2.0f).withLayer();
-
-
-
-
-
-        //scaleX(toWidth);
-        //사이즈 변화
-        //ScaleAnimation scaleAnimation = new ScaleAnimation(schDetailTopBar.getWidth(),toWidth,schDetailTopBar.getHeight(),toHeigth);
-        //scaleAnimation.setDuration(1000);
-        //scaleAnimation.start();
-        //ObjectAnimator ani = ObjectAnimator.ofFloat(schDetailTopBar,"scaleX",toWidth);
-        //ani.setDuration(1000);
-        //ani.start();
     }
 
     @Override
     public void onGlobalLayout() {
         schDetailTopBar.setX(schDetailWeekDay.getX());
-        //schDetailTopBar.setMaxWidth(schDetailWeekDay.getWidth());
         schDetailTopBar.getLayoutParams().width = schDetailWeekDay.getWidth();
-        //schDetailTopBar.setMaxWidth(schDetailWeekDay.getWidth());
     }
 }
