efcf411b6b253398dda9dd510efbd90160283bae
angalle <gmltjs150@naver.com>
2018-03-12 18:11:37 +0900

    카카오톡 sdk 개인정보 받아오기 해야함

--- app/src/main/java/univ/sm/View/board/login/IndirectLoginView.java
+++ app/src/main/java/univ/sm/View/board/login/IndirectLoginView.java
@@ -3,12 +3,26 @@ package univ.sm.View.board.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
 import com.kakao.util.exception.KakaoException;
 import com.kakao.util.helper.log.Logger;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 import univ.sm.R;
 import univ.sm.View.CommonView;
@@ -20,9 +34,12 @@ import univ.sm.View.CommonView;
 public class IndirectLoginView extends CommonView implements View.OnClickListener{
     private SessionCallback callback;
 
+    private CallbackManager callbackManager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.indirect_login);
 
         //KakaoSDK.init(new KakaoTalkSdkAdapter());
@@ -31,11 +48,45 @@ public class IndirectLoginView extends CommonView implements View.OnClickListene
         Session.getCurrentSession().addCallback(callback);
         Session.getCurrentSession().checkAndImplicitOpen();
 
+
+        callbackManager = CallbackManager.Factory.create();
+        facebook();
+    }
+
+    private void facebook() {
+        LoginButton loginButton = (LoginButton) findViewById(R.id.facebook_login);
+        //loginButton.setReadPermissions( "email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.v("result",object.toString());
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("LoginErr",error.toString());
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
-
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email","user_friends"));
     }
 
     private class SessionCallback implements ISessionCallback {
@@ -60,11 +111,12 @@ public class IndirectLoginView extends CommonView implements View.OnClickListene
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
             return;
         }
-
-        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
